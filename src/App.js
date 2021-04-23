e0e3d52fd5ecdfd7111e56a97d07a6b188601f8b
adityajain-web <60445026+adityajain-web@users.noreply.github.com>
2021-04-23 21:18:18 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+const App = () => {
+  let today = new Date();
+  let time = today.toLocaleTimeString();
+  const [cTime, uTime] = useState(time);
+  const currentTime = () => {
+    let today = new Date();
+    let time = today.toLocaleTimeString();
+    uTime(time);
+  }
+
+  setInterval(currentTime, 1000);
+
+  let greet;
+  let hours = today.getHours();
+
+  if (hours >= 6 && hours < 12) {
+    greet = "Good Morning";
+  } else if (hours >= 12 && hours < 18) {
+    greet = "Good Afternoon";
+  } else if (hours >= 18 && hours < 20) {
+    greet = "Good Evening";
+  } else if (hours >= 20 && hours < 6) {
+    greet = "Good Night";
+  }
+
+  return (<>
+    <div id="container">
+      <div>
+        <h1>{cTime}</h1>
+        <h2>{greet}</h2>
+      </div>
     </div>
-  );
+  </>)
 }
 
 export default App;
